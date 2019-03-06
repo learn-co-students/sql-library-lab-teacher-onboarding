741a9b43e89cf7e4b730ac25137d034cee6698b2
Kyle Coberly <kyle.coberly@gmail.com>
2019-03-06 10:52:05 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,83 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES (
+    'a', 1, 1
+),(
+    'a', 1, 1
+);
+
+INSERT INTO books (title, year, series_id) VALUES (
+    'a', 1, 1
+),(
+    'a', 1, 1
+),(
+    'a', 1, 1
+),(
+    'a', 1, 1
+),(
+    'a', 1, 1
+),(
+    'a', 1, 1
+);
+
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES (
+    'a', 'a', 'a', 1, 1
+), (
+    'a', 'a', 'a', 1, 1
+), (
+    'a', 'a', 'a', 1, 1
+), (
+    'a', 'a', 'a', 1, 1
+), (
+    'a', 'a', 'a', 1, 1
+), (
+    'a', 'a', 'a', 1, 1
+), (
+    'a', 'a', 'a', 1, 1
+), (
+    'a', 'a', 'a', 1, 1
+);
+
+INSERT INTO subgenres (name) VALUES (
+    'a'
+), (
+    'a'
+);
+
+INSERT INTO authors (name) VALUES (
+    'a'
+), (
+    'a'
+);
+
+INSERT INTO character_books (character_id, book_id) VALUES (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+), (
+    1, 1
+);
